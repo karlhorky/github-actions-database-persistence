ae4c5b356f8ed4a2717a91d121cda68ce1767231
Karl Horky <karl.horky@gmail.com>
2023-03-12 18:56:50 +0100

    Remove Prettier dependencies, suggest extension
    To avoid extra project setup in every new project for package.json key sorting

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('prettier').Options} */
 const config = {
-  plugins: [require.resolve('prettier-plugin-packagejson')],
   // Avoid excessive diffs on changes to MDX files
   proseWrap: 'never',
   singleQuote: true,
